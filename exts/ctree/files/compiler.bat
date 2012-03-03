704c3601c2cdcbd7745870182ce1e9c3600c574f
kobezzza <kobezzza@mail.ru>
2012-03-03 20:59:40 +0400

    up

--- exts/ctree/files/compiler.bat
+++
@@ -1,3 +0,0 @@
-java -jar compiler.jar --js jquery.ctree.js --js_output_file jquery.ctree.gcc.js
-copy txt\head.compile.txt + jquery.ctree.gcc.js + txt\end.txt jquery.ctree.min.js
-del jquery.ctree.gcc.js
