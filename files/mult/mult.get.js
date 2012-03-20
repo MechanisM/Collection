9a91b907c821ca9daca6774f654a05624c50941b
kobezzza <kobezzza@mail.ru>
2012-03-20 12:18:31 +0400

    up

--- files/mult/mult.get.js
+++ files/mult/mult.get.js
@@ -1,86 +1,1 @@
-	
-	/////////////////////////////////
-	//// mult methods (get)
-	/////////////////////////////////
-	
-	/**
-	 * get the elements using a filter or by link (in context)
-	 * 
-	 * @this {Colletion Object}
-	 * @param {Filter|Context|Boolean} [filter=this.ACTIVE] — filter function, string expression (the record is equivalent to: return + string expression), context (overload) or true (if disabled)
-	 * @param {String} [id=this.ACTIVE] — collection ID, if the id is a Boolean, it is considered as mult
-	 * @param {Boolean} [mult=true] — if false, then there will only be one iteration
-	 * @param {Number|Boolean} [count=false] — maximum number of results (by default: all object)
-	 * @param {Number|Boolean} [from=false] — skip a number of elements (by default: 0)
-	 * @param {Number|Boolean} [indexOf=false] — starting point (by default: 0)
-	 * @return {mixed}
-	 *
-	 * @example
-	 * $C([{a: 1}, {b: 2}, {c: 3}, {a: 1}, {b: 2}, {c: 3}])
-	 *	.get('eq(-1) > c');
-	 * @example
-	 * $C([{a: 1}, {b: 2}, {c: 3}, {a: 1}, {b: 2}, {c: 3}])
-	 *	.get(':i % 3 === 0');
-	 * @example
-	 * $C([{a: 1}, {b: 2}, {c: 3}, {a: 1}, {b: 2}, {c: 3}])
-	 *	.get(function (el, key, data, i) { return i % 3 === 0; });
-	 */
-	Collection.prototype.get = function (filter, id, mult, count, from, indexOf) {
-		// overload
-		if (Collection.isNumber(filter) || (arguments.length <= 2 && !Collection.isBoolean(id) && Collection.isString(filter)
-			&& !this._filterTest(filter)) || arguments.length === 0 || filter === false) {
-				return this._getOne(filter, id || '');
-			}
-		
-		// if id is Boolean (overload)
-		if (Collection.isBoolean(id)) {
-			indexOf = from;
-			from = count;
-			count = mult;
-			mult = id;
-			id = this.ACTIVE;
-		} else { id = id || ''; }
-	
-		// values by default
-		mult = mult === false ? false : true;
-		count = parseInt(count) >= 0 ? parseInt(count) : false;
-		from = parseInt(from) || false;
-		indexOf = parseInt(indexOf) || false;
-		
-		var result = mult === true ? [] : -1,
-			
-			/** @private */
-			action = function (el, key, data, i, length, cObj, id) {
-				if (mult === true) {
-					result.push(data[key]);
-				} else { result = data[key]; }
-	
-				return true;
-			};
-		
-		this.forEach(action, filter || '', id, mult, count, from, indexOf);
-	
-		return result;
-	};
-	/**
-	 * get the one element using a filter or by link (in context)
-	 * 
-	 * @this {Colletion Object}
-	 * @param {Filter|String|Boolean|Context} [filter=this.ACTIVE] — filter function, string expression or true (if disabled)
-	 * @param {String} [id=this.ACTIVE] — collection ID
-	 * @param {Number|Boolean} [from=false] — skip a number of elements (by default: 0)
-	 * @param {Number|Boolean} [indexOf=false] — starting point (by default: 0)
-	 * @return {mixed}
-	 *
-	 * @example
-	 * $C([{a: 1}, {b: 2}, {c: 3}, {a: 1}, {b: 2}, {c: 3}])
-	 *	.getOne(':i % 3 === 0');
-	 * @example
-	 * $C([{a: 1}, {b: 2}, {c: 3}, {a: 1}, {b: 2}, {c: 3}])
-	 *	.getOne(function (el, key, data, i) {
-	 *		return i % 3 === 0;
-	 *	});
-	 */
-	Collection.prototype.getOne = function (filter, id, from, indexOf) {
-		return this.get(filter || '', id || '', false, '', from || '', indexOf || '');
-	};
+		/////////////////////////////////	//// mult methods (get)	/////////////////////////////////		/**	 * get the elements using a filter or by link (in context)	 * 	 * @this {Colletion Object}	 * @param {Filter|Context|Boolean} [filter=this.ACTIVE] — filter function, string expression (the record is equivalent to: return + string expression), context (overload) or true (if disabled)	 * @param {String} [id=this.ACTIVE] — collection ID, if the id is a Boolean, it is considered as mult (overload)	 * @param {Boolean} [mult=true] — if false, then there will only be one iteration	 * @param {Number|Boolean} [count=false] — maximum number of results (by default: all object)	 * @param {Number|Boolean} [from=false] — skip a number of elements (by default: 0)	 * @param {Number|Boolean} [indexOf=false] — starting point (by default: 0)	 * @return {mixed}	 *	 * @example	 * $C([{a: 1}, {b: 2}, {c: 3}, {a: 1}, {b: 2}, {c: 3}])	 *	.get('eq(-1) > c');	 * @example	 * $C([{a: 1}, {b: 2}, {c: 3}, {a: 1}, {b: 2}, {c: 3}])	 *	.get(':i % 3 === 0');	 * @example	 * $C([{a: 1}, {b: 2}, {c: 3}, {a: 1}, {b: 2}, {c: 3}])	 *	.get(function (el, key, data, i) { return i % 3 === 0; });	 */	Collection.prototype.get = function (filter, id, mult, count, from, indexOf) {		// overload		if (Collection.isNumber(filter) || (arguments.length <= 2 && !Collection.isBoolean(id) && Collection.isString(filter)			&& !this._filterTest(filter)) || arguments.length === 0 || filter === false) {				return this._getOne(filter, id || '');			}				// if id is Boolean (overload)		if (Collection.isBoolean(id)) {			indexOf = from;			from = count;			count = mult;			mult = id;			id = this.ACTIVE;		} else { id = id || ''; }			// values by default		mult = mult === false ? false : true;		count = parseInt(count) >= 0 ? parseInt(count) : false;		from = parseInt(from) || false;		indexOf = parseInt(indexOf) || false;				var result = mult === true ? [] : -1,						/** @private */			action = function (el, key, data, i, length, cObj, id) {				if (mult === true) {					result.push(data[key]);				} else { result = data[key]; }					return true;			};				this.forEach(action, filter || '', id, mult, count, from, indexOf);			return result;	};	/**	 * get the one element using a filter or by link (in context)	 * 	 * @this {Colletion Object}	 * @param {Filter|String|Boolean|Context} [filter=this.ACTIVE] — filter function, string expression or true (if disabled)	 * @param {String} [id=this.ACTIVE] — collection ID	 * @param {Number|Boolean} [from=false] — skip a number of elements (by default: 0)	 * @param {Number|Boolean} [indexOf=false] — starting point (by default: 0)	 * @return {mixed}	 *	 * @example	 * $C([{a: 1}, {b: 2}, {c: 3}, {a: 1}, {b: 2}, {c: 3}])	 *	.getOne(':i % 3 === 0');	 * @example	 * $C([{a: 1}, {b: 2}, {c: 3}, {a: 1}, {b: 2}, {c: 3}])	 *	.getOne(function (el, key, data, i) {	 *		return i % 3 === 0;	 *	});	 */	Collection.prototype.getOne = function (filter, id, from, indexOf) {		return this.get(filter || '', id || '', false, '', from || '', indexOf || '');	};
--- files/mult/mult.search.js
+++ files/mult/mult.search.js
@@ -1,132 +1,1 @@
-	
-	/////////////////////////////////
-	//// mult methods (search)
-	/////////////////////////////////
-	
-	/**
-	 * search for elements using filter (returns a reference to elements) (in context)
-	 * 
-	 * @this {Colletion Object}
-	 * @param {Filter|Boolean} [filter=this.ACTIVE] — filter function, string expression (the record is equivalent to: return + string expression) or true (if disabled)
-	 * @param {String} [id=this.ACTIVE] — collection ID, if the id is a Boolean, it is considered as mult
-	 * @param {Boolean} [mult=true] — if false, then there will only be one iteration
-	 * @param {Number|Boolean} [count=false] — maximum number of results (by default: all object)
-	 * @param {Number|Boolean} [from=false] — skip a number of elements (by default: 0)
-	 * @param {Number|Boolean} [indexOf=false] — starting point (by default: 0)
-	 * @return {Number|Array}
-	 *
-	 * @example
-	 * $C([{a: 1}, {b: 2}, {c: 3}, {a: 1}, {b: 2}, {c: 3}])
-	 *	.search(':i % 3 === 0');
-	 * @example
-	 * $C([{a: 1}, {b: 2}, {c: 3}, {a: 1}, {b: 2}, {c: 3}])
-	 *	.search(function (el, key, data, i) { return i % 3 === 0; });
-	 */
-	Collection.prototype.search = function (filter, id, mult, count, from, indexOf) {
-		// if id is Boolean (overload)
-		if (Collection.isBoolean(id)) {
-			indexOf = from;
-			from = count;
-			count = mult;
-			mult = id;
-			id = this.ACTIVE;
-		} else { id = id || ''; }
-	
-		// values by default
-		mult = mult === false ? false : true;
-		count = parseInt(count) >= 0 ? parseInt(count) : false;
-		from = parseInt(from) || false;
-		indexOf = parseInt(indexOf) || false;
-		
-		var result = mult === true ? [] : -1,
-			
-			/** @private */
-			action = function (el, key, data, i, length, cObj, id) {
-				if (mult === true) {
-					result.push(key);
-				} else { result = key; }
-				
-				return true;
-			};
-		
-		this.forEach(action, filter || '', id, mult, count, from, indexOf);
-		
-		return result;
-	};
-	/**
-	 * search for one element using filter (returns a reference to element) (in context)
-	 *
-	 * @this {Colletion Object}
-	 * @param {Filter|Boolean} [filter=this.ACTIVE] — filter function, string expression (the record is equivalent to: return + string expression) or true (if disabled)
-	 * @param {String} [id=this.ACTIVE] — collection ID
-	 * @param {Number|Boolean} [from=false] — skip a number of elements (by default: 0)
-	 * @param {Number|Boolean} [indexOf=false] — starting point (by default: 0)
-	 * @return {Number|Array}
-	 *
-	 * @example
-	 * $C([{a: 1}, {b: 2}, {c: 3}, {a: 1}, {b: 2}, {c: 3}])
-	 *	.searchOne(':i % 3 === 0');
-	 * @example
-	 * $C([{a: 1}, {b: 2}, {c: 3}, {a: 1}, {b: 2}, {c: 3}])
-	 *	.searchOne(function (el, key, data, i) { return i % 3 === 0; });
-	 */
-	Collection.prototype.searchOne = function (filter, id, from, indexOf) {
-		return this.search(filter || '', id || '', false, '', from || '', indexOf || '');
-	};
-	
-	/**
-	 * returns the first index/key at which a given element can be found in the collection (in context)
-	 * 
-	 * @this {Colletion Object}
-	 * @param {mixed} searchElement — element to locate in the array
-	 * @param {fromIndex} [fromIndex=0] — the index at which to start searching backwards
-	 * @param {String} [id=this.ACTIVE] — collection ID
-	 * @return {Number|String}
-	 *
-	 * @example
-	 * $C([1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5]).indexOf(1);
-	 * @example
-	 * $C([1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5]).indexOf(1, 2);
-	 */
-	Collection.prototype.indexOf = function (searchElement, fromIndex, id) {
-		id = id || '';
-		fromIndex = fromIndex || '';
-		
-		var data = Collection.byLink(this._get('collection', id), this._getActiveParam('context'));
-		
-		if (Collection.isArray(data) && data.indexOf) {
-			if (fromIndex) { return data.indexOf(searchElement, fromIndex); }
-			
-			return data.indexOf(searchElement);
-		} else { return this.search(function (el) { return el === searchElement; }, id, false, '', '', fromIndex); }
-	};
-	/**
-	 * returns the last index/key at which a given element can be found in the collection (in context)
-	 * 
-	 * @this {Colletion Object}
-	 * @param {mixed} searchElement — element to locate in the array
-	 * @param {fromIndex} [fromIndex=0] — the index at which to start searching backwards
-	 * @param {String} [id=this.ACTIVE] — collection ID
-	 * @return {Number|String}
-	 *
-	 * @example
-	 * $C([1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5]).lastIndexOf(1);
-	 * @example
-	 * $C([1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5]).lastIndexOf(1, 2);
-	 */
-	Collection.prototype.lastIndexOf = function (searchElement, fromIndex, id) {
-		id = id || '';
-		fromIndex = fromIndex || '';
-		
-		var el, data = Collection.byLink(this._get('collection', id), this._getActiveParam('context'));
-		
-		if (Collection.isArray(data) && data.lastIndexOf) {
-			if (fromIndex) { return data.lastIndexOf(searchElement, fromIndex); }
-			
-			return data.lastIndexOf(searchElement);
-		} else {
-			el = this.search(function (el) { return el === searchElement; }, id, '', '', '', fromIndex);
-			
-			return typeof el[el.length - 1] !== 'undefined' ? el[el.length - 1] : -1;
-		}
-	};
+		/////////////////////////////////	//// mult methods (search)	/////////////////////////////////		/**	 * search for elements using filter (returns a reference to elements) (in context)	 * 	 * @this {Colletion Object}	 * @param {Filter|Boolean} [filter=this.ACTIVE] — filter function, string expression (the record is equivalent to: return + string expression) or true (if disabled)	 * @param {String} [id=this.ACTIVE] — collection ID, if the id is a Boolean, it is considered as mult (overload)	 * @param {Boolean} [mult=true] — if false, then there will only be one iteration	 * @param {Number|Boolean} [count=false] — maximum number of results (by default: all object)	 * @param {Number|Boolean} [from=false] — skip a number of elements (by default: 0)	 * @param {Number|Boolean} [indexOf=false] — starting point (by default: 0)	 * @return {Number|Array}	 *	 * @example	 * $C([{a: 1}, {b: 2}, {c: 3}, {a: 1}, {b: 2}, {c: 3}])	 *	.search(':i % 3 === 0');	 * @example	 * $C([{a: 1}, {b: 2}, {c: 3}, {a: 1}, {b: 2}, {c: 3}])	 *	.search(function (el, key, data, i) { return i % 3 === 0; });	 */	Collection.prototype.search = function (filter, id, mult, count, from, indexOf) {		// if id is Boolean (overload)		if (Collection.isBoolean(id)) {			indexOf = from;			from = count;			count = mult;			mult = id;			id = this.ACTIVE;		} else { id = id || ''; }			// values by default		mult = mult === false ? false : true;		count = parseInt(count) >= 0 ? parseInt(count) : false;		from = parseInt(from) || false;		indexOf = parseInt(indexOf) || false;				var result = mult === true ? [] : -1,						/** @private */			action = function (el, key, data, i, length, cObj, id) {				if (mult === true) {					result.push(key);				} else { result = key; }								return true;			};				this.forEach(action, filter || '', id, mult, count, from, indexOf);				return result;	};	/**	 * search for one element using filter (returns a reference to element) (in context)	 *	 * @this {Colletion Object}	 * @param {Filter|Boolean} [filter=this.ACTIVE] — filter function, string expression (the record is equivalent to: return + string expression) or true (if disabled)	 * @param {String} [id=this.ACTIVE] — collection ID	 * @param {Number|Boolean} [from=false] — skip a number of elements (by default: 0)	 * @param {Number|Boolean} [indexOf=false] — starting point (by default: 0)	 * @return {Number|Array}	 *	 * @example	 * $C([{a: 1}, {b: 2}, {c: 3}, {a: 1}, {b: 2}, {c: 3}])	 *	.searchOne(':i % 3 === 0');	 * @example	 * $C([{a: 1}, {b: 2}, {c: 3}, {a: 1}, {b: 2}, {c: 3}])	 *	.searchOne(function (el, key, data, i) { return i % 3 === 0; });	 */	Collection.prototype.searchOne = function (filter, id, from, indexOf) {		return this.search(filter || '', id || '', false, '', from || '', indexOf || '');	};		/**	 * returns the first index/key at which a given element can be found in the collection (in context)	 * 	 * @this {Colletion Object}	 * @param {mixed} searchElement — element to locate in the array	 * @param {fromIndex} [fromIndex=0] — the index at which to start searching backwards	 * @param {String} [id=this.ACTIVE] — collection ID	 * @return {Number|String}	 *	 * @example	 * $C([1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5]).indexOf(1);	 * @example	 * $C([1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5]).indexOf(1, 2);	 */	Collection.prototype.indexOf = function (searchElement, fromIndex, id) {		id = id || '';		fromIndex = fromIndex || '';				var data = Collection.byLink(this._get('collection', id), this._getActiveParam('context'));				if (Collection.isArray(data) && data.indexOf) {			if (fromIndex) { return data.indexOf(searchElement, fromIndex); }						return data.indexOf(searchElement);		} else { return this.search(function (el) { return el === searchElement; }, id, false, '', '', fromIndex); }	};	/**	 * returns the last index/key at which a given element can be found in the collection (in context)	 * 	 * @this {Colletion Object}	 * @param {mixed} searchElement — element to locate in the array	 * @param {fromIndex} [fromIndex=0] — the index at which to start searching backwards	 * @param {String} [id=this.ACTIVE] — collection ID	 * @return {Number|String}	 *	 * @example	 * $C([1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5]).lastIndexOf(1);	 * @example	 * $C([1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5]).lastIndexOf(1, 2);	 */	Collection.prototype.lastIndexOf = function (searchElement, fromIndex, id) {		id = id || '';		fromIndex = fromIndex || '';				var el, data = Collection.byLink(this._get('collection', id), this._getActiveParam('context'));				if (Collection.isArray(data) && data.lastIndexOf) {			if (fromIndex) { return data.lastIndexOf(searchElement, fromIndex); }						return data.lastIndexOf(searchElement);		} else {			el = this.search(function (el) { return el === searchElement; }, id, '', '', '', fromIndex);						return typeof el[el.length - 1] !== 'undefined' ? el[el.length - 1] : -1;		}	};
